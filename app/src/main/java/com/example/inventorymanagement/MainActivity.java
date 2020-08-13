17dd504b73a37b4bd62581f988694a8010b6099b
srimantini2000 <srimantini2019@gmail.com>
2020-08-14 03:57:40 +0530

    new commit

--- app/src/main/java/com/example/inventorymanagement/MainActivity.java
+++ app/src/main/java/com/example/inventorymanagement/MainActivity.java
@@ -10,12 +10,14 @@ import android.os.Bundle;
 import android.view.View;
 
 import android.widget.Button;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button adminButton = findViewById(R.id.adminButton);
+        TextView adminButton = findViewById(R.id.adminButton);
         Button workButton = findViewById(R.id.workButton);
 
 
